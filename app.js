19025d51750f42bcce332097fbf537f92d53707f
seregaMark <markovskij.ser@yandex.ru>
2024-04-22 18:39:58 +0300

    обработка всех статичных файлов
    цсс роботает

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const PORT = 3005;
 
 const fs = require('fs').promises;
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const data = await fs.readFile('./public/index.html', 'utf-8')
-    res.getHeader('Content-Type', 'text/html');
-    res.send(data);
-});
+// app.get('/', async (req, res) => {
+//     const data = await fs.readFile('./public/index.html', 'utf-8')
+//     res.getHeader('Content-Type', 'text/html');
+//     res.send(data);
+// });
 
-app.get('/styles/style.css', async (req, res) => {
-    const data = await fs.readFile('./public/styles/style.css', 'utf-8')
-    res.getHeader('Content-Type', 'text/css');
-    res.send(data);
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`server popushen ${PORT}`);
