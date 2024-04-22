f425477a8a81d84f09217307bc1ffd8ace982a46
seregaMark <markovskij.ser@yandex.ru>
2024-04-22 23:21:29 +0300

    закончил работу

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,6 @@
 function cors(req, res, next) {
     const { origin } = req.headers;
     res.header('Access-Control-Allow-Origin', 'localhost:3005');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     next();
 }
 
